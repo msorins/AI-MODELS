87c4bab8ba7dd014939f0013464d67676a14ff8a
FIXED-TERM Mircea Sorin-Sebastian (RBRO/ESA1) <mss4clj@bosch.com>
2017-08-03 16:26:44 +0300

    Fixed a mistake in 5th model where I used the train data as gray scaled images instead of coloured ones + a new generator that supports randomise multiple coloured shapes

--- 05v3numberOfObjectsPredictionAugmentedByFolders.py
+++ 05v3numberOfObjectsPredictionAugmentedByFolders.py
@@ -3,50 +3,45 @@ os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID" # see issue #152
 os.environ["CUDA_VISIBLE_DEVICES"]="1,2,3" # Will use only 2nd, 3rd and 4th gpu
 
 #Part 1 -> Importing
+from keras.datasets import cifar10
 from keras.models import Sequential
-from keras.layers import Convolution2D
-from keras.layers import MaxPooling2D
-from keras.layers import Flatten
 from keras.layers import Dense
 from keras.layers import Dropout
-from keras.wrappers.scikit_learn import KerasRegressor
+from keras.layers import Flatten
+from keras.constraints import maxnorm
+from keras.optimizers import SGD
+from keras.layers.convolutional import Convolution2D
+from keras.layers.convolutional import MaxPooling2D
+from keras.utils import np_utils
 from keras import backend as K
 K.set_image_dim_ordering('th')
-
 
 #Part 2 -> Initialise the CNN
 
 classifier = Sequential()
-
-# Step 1 -> Convolution
-classifier.add(Convolution2D(32, 3, 3, input_shape = (3, 64, 64), activation = 'relu'))
-
-# Step 2 -> Pooling
-classifier.add(MaxPooling2D(pool_size = (2, 2)))
-
+classifier.add(
+    Convolution2D(32, 3, 3, input_shape=(3, 64, 64), activation='relu')
+)
 classifier.add(Dropout(0.2))
-
-# Step 3 -> Convolution
-classifier.add(Convolution2D(32, 3, 3,  activation = 'relu'))
-
-# Step 4 -> Pooling
-classifier.add(MaxPooling2D(pool_size = (2, 2)))
-
+classifier.add(Convolution2D(32, 3, 3, activation='relu'))
+classifier.add(MaxPooling2D(pool_size=(2, 2)))
+classifier.add(Convolution2D(64, 3, 3, activation='relu'))
 classifier.add(Dropout(0.2))
-
-classifier.add(Convolution2D(32, 3, 3,  activation = 'relu'))
-
-# Step 4 -> Pooling
-classifier.add(MaxPooling2D(pool_size = (2, 2)))
-
-
-# Step 5 -> Flattening
+classifier.add(Convolution2D(64, 3, 3, activation='relu'))
+classifier.add(MaxPooling2D(pool_size=(2, 2)))
+classifier.add(Convolution2D(128, 3, 3, activation='relu'))
+classifier.add(Dropout(0.2))
+classifier.add(Convolution2D(128, 3, 3, activation='relu'))
+classifier.add(MaxPooling2D(pool_size=(2, 2)))
+classifier.add(Convolution2D(256, 3, 3, activation='relu'))
+classifier.add(MaxPooling2D(pool_size=(2, 2)))
 classifier.add(Flatten())
-
-# Step 6 -> Full connection
-classifier.add(Dense(output_dim = 256, activation = 'relu'))
-classifier.add(Dense(output_dim = 512, activation = 'relu'))
-classifier.add(Dense(output_dim = 256, activation = 'relu'))
+classifier.add(Dropout(0.2))
+classifier.add(Dense(2048, activation='relu'))
+classifier.add(Dropout(0.2))
+classifier.add(Dense(512, activation='relu'))
+classifier.add(Dense(1024, activation='relu'))
+classifier.add(Dropout(0.2))
 classifier.add(Dense(output_dim = 1, activation = 'relu'))
 
 #Part 3 -> Compile the CNN
@@ -62,22 +57,34 @@ train_datagen = ImageDataGenerator(rescale = 1./255,
 
 test_datagen = ImageDataGenerator(rescale = 1./255)
 
-training_set = train_datagen.flow_from_directory('datasetv3/training_set',
+training_set = train_datagen.flow_from_directory('datasetv4/training_set',
                                                  target_size = (64, 64),
-                                                 batch_size = 32,
+                                                 batch_size = 64,
                                                  class_mode = 'sparse')
 
-test_set = test_datagen.flow_from_directory('datasetv3/test_set',
+test_set = test_datagen.flow_from_directory('datasetv4/test_set',
                                             target_size = (64, 64),
-                                            batch_size = 32,
-                                            class_mode = 'sparse')
+                                            batch_size = 64,
+                                            class_mode = 'sparse',)
 
 
 #Part 5 -> fit the  NN
+"""
 classifier.fit_generator(training_set,
-                         samples_per_epoch = 8000,
-                         nb_epoch = 20,
+                         samples_per_epoch = 2880,
+                         nb_epoch = 100,
                          validation_data = test_set,
-                         nb_val_samples = 2000)
+                         nb_val_samples = 1408)
+"""
 
-classifier.save(filepath = os.getcwd() + "/" + "savedModel.data")
+crtEpoch = 0
+maxEpoch = 1500
+while crtEpoch <= maxEpoch:
+    classifier.fit_generator(training_set,
+                             samples_per_epoch = 2880,
+                             nb_epoch = 1,
+                             validation_data = test_set,
+                             nb_val_samples = 1408)
+
+    classifier.save_weights(filepath = os.getcwd() + "/" + "savedModel-e" + str(crtEpoch) +".weights")
+
--- 07LoadAndUseModel.py
+++ 07LoadAndUseModel.py
@@ -1,48 +1,75 @@
 import glob
 from keras.models import load_model
 from numpy import genfromtxt
+import numpy
 import cv2
 import os
 import h5py
+from decimal import Decimal
+import matplotlib.pyplot as plt
 
 #Global variables
 cwd = os.getcwd()
-img_cols, img_rows = 256, 256
+img_cols, img_rows = 64, 64
 
 def get_im(path):
     # Load as grayscale
     print("Path: ", path)
-    img = cv2.imread(path, 0)
+    img = cv2.imread(path)
     # Reduce size
     resized = cv2.resize(img, (img_cols, img_rows))
     return resized
 
-def load_images(path):
-    images = []
+def load_images_predict(model, path):
+    res = ''
     files = glob.glob(path)
 
+    crt = 0
     for fl in files:
         if "res" in fl:
             continue
 
+        #Load the image
         fl = cwd + "/" + fl
         img = get_im(fl)
-        images.append(img)
 
-    return images
+        #Predict a result
+        img = numpy.array(img)
+        img = numpy.reshape(img, (1, img_cols, img_rows, 3))
+        prediction = model.predict(x = img)
+        prediction = prediction[0][0]
 
-def evaluate_prediction(model, images):
-    for crtImg in images:
-        print( model.evaluate(crtImg) )
+        #Scatter the result
+        plt.scatter(x=prediction * 1000000000, y = 0)
+
+        #Save the result
+        res += str(fl) + " => prediction: " + str( prediction ) + "\n"
+
+        crt += 1
+        if crt >= 350:
+            break
+
+
+    plt.show()
+
+    return res
+
+def save_string_to_file(string, path):
+    f = open(path, 'w')
+    f.write(string)
+    f.close()
 
 def main():
     # Load the model
     model = load_model('07isMushroom.h5')
 
-    # Load each image folder and evaluate
-    imgs = load_images(os.path.join('mushrooms', 'goodmushrooms', '*.png'))
-    evaluate_prediction(model, imgs)
+    # Load each image folder and predict
+    predictionsGoodMushrooms = load_images_predict(model, os.path.join('mushrooms', 'GoodMushrooms', '*.jpg'))
+    save_string_to_file(predictionsGoodMushrooms, './GoodMushroomsPredictions.txt')
 
+    # Load each image folder and predict
+    predictionsBadMushrooms = load_images_predict(model, os.path.join('mushrooms', 'BadMushrooms', '*.jpg'))
+    save_string_to_file(predictionsBadMushrooms, './BadMushroomsPredictions.txt')
     
 #Execute the main function    
 main()
