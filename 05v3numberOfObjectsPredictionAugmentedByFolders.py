0237decd57a076dda0e978cbc00fcdbbc24ef936
FIXED-TERM Mircea Sorin-Sebastian (RBRO/ESA1) <mss4clj@bosch.com>
2017-08-07 17:40:01 +0300

    Started to work at RNN's

--- 05v3numberOfObjectsPredictionAugmentedByFolders.py
+++ 05v3numberOfObjectsPredictionAugmentedByFolders.py
@@ -1,3 +1,4 @@
+# 88 percent accuracy
 import os
 os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID" # see issue #152
 os.environ["CUDA_VISIBLE_DEVICES"]="2,3" # Will use only 2nd, 3rd and 4th gpu
@@ -22,32 +23,40 @@ classifier = Sequential()
 classifier.add(
     Convolution2D(32, 3, 3, input_shape=(3, 64, 64), activation='relu')
 )
+classifier.add(Dropout(0.2))
 classifier.add(Convolution2D(32, 3, 3, activation='relu'))
 classifier.add(MaxPooling2D(pool_size=(2, 2)))
 classifier.add(Convolution2D(64, 3, 3, activation='relu'))
+classifier.add(Dropout(0.2))
 classifier.add(Convolution2D(64, 3, 3, activation='relu'))
 classifier.add(MaxPooling2D(pool_size=(2, 2)))
 classifier.add(Convolution2D(128, 3, 3, activation='relu'))
+classifier.add(Dropout(0.2))
 classifier.add(Convolution2D(128, 3, 3, activation='relu'))
+classifier.add(MaxPooling2D(pool_size=(2, 2)))
+classifier.add(Convolution2D(256, 3, 3, activation='relu'))
 classifier.add(MaxPooling2D(pool_size=(2, 2)))
 classifier.add(Flatten())
 classifier.add(Dropout(0.2))
 classifier.add(Dense(2048, activation='relu'))
 classifier.add(Dropout(0.2))
-classifier.add(Dense(512, activation='relu'))
 classifier.add(Dense(1024, activation='relu'))
+classifier.add(Dense(2048, activation='relu'))
 classifier.add(Dropout(0.2))
 classifier.add(Dense(output_dim = 1, activation = 'relu'))
 
 #Part 3 -> Compile the CNN
-classifier.compile(optimizer = 'adam', loss = 'mse', metrics = ['accuracy'])
+classifier.compile(optimizer = 'adadelta', loss = 'mse', metrics = ['accuracy'])
 
 #Part 4 -> Get the images
 from keras.preprocessing.image import ImageDataGenerator
 
-train_datagen = ImageDataGenerator()
+train_datagen = ImageDataGenerator(rescale = 1./255,
+                                   shear_range = 0.2,
+                                   zoom_range = 0.2,
+                                   horizontal_flip = True)
 
-test_datagen = ImageDataGenerator()
+test_datagen = ImageDataGenerator(rescale = 1./255)
 
 training_set = train_datagen.flow_from_directory('datasetv5/training_set',
                                                  target_size = (64, 64),
@@ -63,10 +72,10 @@ test_set = test_datagen.flow_from_directory('datasetv5/test_set',
 #Part 5 -> fit the  NN
 
 classifier.fit_generator(training_set,
-                         samples_per_epoch = 5952,
+                         samples_per_epoch = 8000,
                          nb_epoch = 120,
                          validation_data = test_set,
-                         nb_val_samples = 1984)
+                         nb_val_samples = 3968)
 """
 crtEpoch = 0
 maxEpoch = 1500
